29a25f52eee32b81df4666ea907912b01c6f6faf
Michael Hatch <michael.hatch@gifttree.com>
2020-10-11 08:30:16 -0700

    more cleanup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "./services/registerServiceWorker";
 import "./index.css";
+import "./services/registerServiceWorker";
 
 createApp(App).use(store).use(router).mount("#app");
