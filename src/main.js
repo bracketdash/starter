0b305ba8e0bb2081223107b9012e6b174a99d9b1
Michael Hatch <michael.hatch@gifttree.com>
2020-10-10 18:17:40 -0700

    move service worker into services directory

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "./services/registerServiceWorker";
 import "./index.css";
 
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount("#app");
+createApp(App).use(store).use(router).mount("#app");
