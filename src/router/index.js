3bd8a3025ed007cd1228a1537589a5d40049d5b7
bracketdash <michaelhatch@me.com>
2020-10-10 21:08:32 -0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: function() {
-            return import("../views/About.vue");
-        },
-    },
+    { path: "/", component: Home },
+    { path: "/about", component: About }
 ];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes,
+    routes
 });
 
 export default router;
