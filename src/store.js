65d2009843de259a2e61276d90cba81b8a394a76
bracketdash <michaelhatch@me.com>
2020-10-28 14:10:07 -0700

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,13 +1,9 @@
-import { createStore } from "vuex";
-
-const state = {
+export const state = {
     message: "Hello, World!",
 };
 
-const mutations = {
+export const mutations = {
     setMessage(state, message) {
         state.message = message;
     },
 };
-
-export default createStore({ state, mutations });
