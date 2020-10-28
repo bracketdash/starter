a38bb15f8bb018037c93f0cdbea4766c4b27593d
bracketdash <michaelhatch@me.com>
2020-10-28 13:59:50 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import "./assets/global.scss";
 
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 import App from "./components/App.vue";
 
-import router from "./router";
+import routes from "./routes";
 import store from "./store";
+
+const history = createWebHistory(process.env.BASE_URL);
+const router = createRouter({ history, routes });
 
 createApp(App).use(store).use(router).mount("#app");
