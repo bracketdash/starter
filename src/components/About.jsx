e5949116f07b8b11b123316ffbc6cf88fbf9f4dd
bracketdash <michaelhatch@me.com>
2020-10-28 22:23:10 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,15 @@
 import ChangeMessage from "./ChangeMessage";
 
-export default {
-    name: "About",
-    setup() {
-        return () => (
-            <div>
-                <div>
-                    <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <h1>This is the about page.</h1>
-                <ChangeMessage />
-            </div>
-        );
-    },
-};
+const name = "About";
+
+const setup = () => () => (
+    <div>
+        <div>
+            <i class="fas fa-info-circle fa-3x"></i>
+        </div>
+        <h1>This is the about page.</h1>
+        <ChangeMessage />
+    </div>
+);
+
+export default { name, setup };
