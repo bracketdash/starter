31377d2ca9f2d066b2be69f09abafadd67b23eb8
bracketdash <michaelhatch@me.com>
2020-10-28 21:57:02 -0700

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { reactive } from "vue";
 import routes from "./routes";
-import sharedState from "./sharedState";
+import sharedStateSrc from "./sharedStateSrc";
 
 const history = createWebHistory(process.env.BASE_URL);
 
 export const router = createRouter({ history, routes });
 
-export const sharedState = reactive(sharedState);
+export const sharedState = reactive(sharedStateSrc);
