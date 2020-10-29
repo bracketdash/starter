273be839db03929971811ac7e347c9a5818fccf8
bracketdash <michaelhatch@me.com>
2020-10-28 22:26:15 -0700

    Update ChangeMessage.jsx

--- src/components/ChangeMessage.jsx
+++ src/components/ChangeMessage.jsx
@@ -1,16 +1,17 @@
 import { sharedState } from "../services";
 
-export default {
-    name: "ChangeMessage",
-    setup() {
-        const setMessage = (e) => {
-            sharedState.message = e.target.value;
-        };
-        return () => (
-            <div>
-                <p>Edit the home page message here:</p>
-                <input value={sharedState.message} onInput={setMessage} />
-            </div>
-        );
-    },
+const name = "ChangeMessage";
+
+const setup = () => {
+    const setMessage = (e) => {
+        sharedState.message = e.target.value;
+    };
+    return () => (
+        <div>
+            <p>Edit the home page message here:</p>
+            <input value={sharedState.message} onInput={setMessage} />
+        </div>
+    );
 };
+
+export default { name, setup };
