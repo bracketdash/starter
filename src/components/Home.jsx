6a6a5b27ecdc3a53cb14842d918d71d64642aa9c
Michael Hatch <michael.hatch@gifttree.com>
2020-10-29 13:06:42 -0700

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import MessageDisplay from "./MessageDisplay";
 import ChangeMessage from "./ChangeMessage";
+import logo from "../assets/images/logo.png";
+import MessageDisplay from "./MessageDisplay";
 
 const name = "Home";
 
 const setup = () => () => (
     <div>
-        <img alt="Vue logo" src="/images/logo.png" />
+        <img alt="Vue logo" src={logo} />
         <MessageDisplay />
         <ChangeMessage />
     </div>
