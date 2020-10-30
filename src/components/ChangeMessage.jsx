ee39914cb703219975e83701c1c5a37efd979773
Michael Hatch <michael.hatch@gifttree.com>
2020-10-29 19:29:21 -0700

    update

--- src/components/ChangeMessage.jsx
+++ src/components/ChangeMessage.jsx
@@ -17,7 +17,7 @@ export default {
             <div>
                 <p>Edit the home page message here:</p>
                 <input value={localState.message} onInput={setLocalMessage} />
-                <div>
+                <div class="update-button">
                     <button onClick={setSharedMessage}>Change "{sharedState.message}" to "{localState.message}"</button>
                 </div>
             </div>
