229a09c2ecd5272607c54d683441821d5d4b61ca
bracketdash <michaelhatch@me.com>
2020-10-29 19:03:29 -0700

    Update ChangeMessage.jsx

--- src/components/ChangeMessage.jsx
+++ src/components/ChangeMessage.jsx
@@ -1,15 +1,25 @@
+import { reactive } from "vue";
 import sharedState from "../services/sharedState";
 
 export default {
     name: "ChangeMessage",
     setup() {
-        const setMessage = (e) => {
-            sharedState.message = e.target.value;
+        const localState = reactive({
+            message: sharedState.message,
+        });
+        const setLocalMessage = (e) => {
+            localState.message = e.target.value;
+        };
+        const setSharedMessage = () => {
+            sharedState.message = localState.message;
         };
         return () => (
             <div>
                 <p>Edit the home page message here:</p>
-                <input value={sharedState.message} onInput={setMessage} />
+                <input value={localState.message} onInput={setLocalMessage} />
+                <div style="text-align: center;">
+                    <button onClick={setSharedMessage}>Save Message</button>
+                </div>
             </div>
         );
     },
