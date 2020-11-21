e15bc5d083fce099ce818edf5a7b66efa884a782
Michael Hatch <michaelhatch@me.com>
2020-11-21 00:51:18 -0800

    Update and rename src/app.js to src/js/index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "@fortawesome/fontawesome-free/css/solid.min.css";
 
-import "./assets/styles.css";
+import "../css/index.css";
 
 import { createApp } from "vue";
 
-import App from "./components/App.vue";
-import router from "./services/router";
-import store from "./services/store";
+import App from "../components/App.vue";
+import router from "../services/router";
+import store from "../services/store";
 
 createApp(App).use(router).use(store).mount("#app");
