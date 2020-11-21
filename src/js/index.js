3b0e0900d5980ee7e10085eda03cc271b0a8d0e6
Michael Hatch <michaelhatch@me.com>
2020-11-21 00:55:38 -0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "@fortawesome/fontawesome-free/css/solid.min.css";
-
 import "../css/index.css";
 
 import { createApp } from "vue";
-
-import App from "../components/App.vue";
-import router from "../services/router";
-import store from "../services/store";
+import App from "../vue/App.vue";
+import router from "./router";
+import store from "./store";
 
 createApp(App).use(router).use(store).mount("#app");
