70df8c763578682b1e87c9ce3faf3c101e4bfe23
Michael Hatch <michaelhatch@me.com>
2020-11-20 17:53:16 -0800

    Update ajax.js

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -1,5 +1,4 @@
-export default (methodAndUrl, ajaxConfig) => {
-  const [method, incomingUrl] = methodAndUrl.split(" ");
+const ajaxFn = (method, incomingUrl, ajaxConfig) => {
   const urlStr = incomingUrl.startsWith("http") ? incomingUrl : window.location.origin + incomingUrl;
   const urlObj = new URL(urlStr);
   const params = new URLSearchParams(urlObj.search);
@@ -16,4 +15,11 @@ export default (methodAndUrl, ajaxConfig) => {
     delete fetchConfig.data;
   }
   return fetch(urlObj.toString(), fetchConfig);
+};
+
+export default {
+  get: (url, config) => ajaxFn("GET", url, config),
+  post: (url, config) => ajaxFn("POST", url, config),
+  put: (url, config) => ajaxFn("PUT", url, config),
+  delete: (url, config) => ajaxFn("DELETE", url, config),
 };
