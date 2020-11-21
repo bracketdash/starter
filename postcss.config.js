dd764fe7baae7ec565f72b588032f887b8c820af
Michael Hatch <michaelhatch@me.com>
2020-11-21 00:43:15 -0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   plugins: {
     "postcss-preset-env": { stage: 1 },
-    tailwindcss: {
-      future: {
-        purgeLayersByDefault: true,
-      },
-      purge: ["./src/**/*.vue"],
-      theme: {
-        extend: {},
-      },
-      variants: {},
-      plugins: [],
-    },
   },
 };
