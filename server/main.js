9e1450bde5d3309a20acb081558a31d17753d4ce
Michael Hatch <michael.hatch@gifttree.com>
2020-11-22 11:22:19 -0800

    notes for later

--- server/main.js
+++ server/main.js
@@ -1,7 +1,13 @@
+const api = require("./api");
 const express = require("express");
 const path = require("path");
 
-const api = require("./api");
+/*
+TODO
+- Make a version of the server that only runs the API without serving ./client/dist
+- Make `yarn dev` also start the API server on a different port
+- Add logic to the front end to point api calls to the API dev server port if running in dev mode
+*/
 
 const app = express();
 
